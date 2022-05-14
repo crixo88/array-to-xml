772847724550defc67fc7bf303ac76a3991c991c
crixo88 <105526152+crixo88@users.noreply.github.com>
2022-05-13 21:49:44 -0300

    comment function and hide test

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <?php
-
+/*
+* Array $data
+* Array $header it's the first line in the xml
+* Array $xml_data used to make the header and first lap
+*/
 function arrayToXml(array $data, ?string $header = '<?xml version="1.0" encoding="utf-8"?>', ?SimpleXMLElement $xml_data = null):?string {
+    // The first time it need to initialize the header and the first tag
     if(!isset($xml_data)){
         $key = array_key_first($data);
         $xml_data = new SimpleXMLElement($header.'<'.$key.'></'.$key.'>');
         arrayToXml($data[$key], null, $xml_data);
+         // for this example we got '<response></response>' after the default header
         return $xml_data->asXML();
     }
+    // After the first round, search for atrubutes or tag
     foreach( $data as $key => $value ) {
+        // if get atributes use the function addAttribute
         if($key == '@attributes'){
             foreach ($value as $attributes => $attrValue) {
                 $xml_data->addAttribute("$attributes",htmlspecialchars("$attrValue"));
             }
             continue;
         }
+        //else search if the tag if a subnode o a primary node
         if( is_array($value) ) {
             if( is_numeric($key) ){
                 $key = 'item'.$key;
@@ -23,10 +32,12 @@ function arrayToXml(array $data, ?string $header = '<?xml version="1.0" encoding
         } else {
             $xml_data->addChild("$key",htmlspecialchars("$value"));
         }
-     }
-     return null;
+    }
+    return null;
 }
 
+/* 
+//for test
 $data = [
     'response' => [
         'error' => [
@@ -36,6 +47,6 @@ $data = [
         ]
     ]
 ];
-
-print_r(arrayToXml($data));
+print_r(arrayToXml($data)); 
+*/
 ?>
